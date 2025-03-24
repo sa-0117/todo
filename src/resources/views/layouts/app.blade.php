d790678cd8b0ba2b44567c5166794c15060be28e
sachiko <axs.r03@gmail.com>
2025-03-25 05:29:21 +0900

    リモートリポジトリの変更

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+                <a class="header__logo" href="/">Todo</a>
+                <nav class="header-nav">
+                    <ul>
+                        <li>
+                            <a class="header-nav__link" href="/categories">カテゴリ一覧</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
